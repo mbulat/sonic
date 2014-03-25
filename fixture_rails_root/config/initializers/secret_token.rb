b496a5b09f8c427be6f0cb73941301f464471d07
Michael Bulat <mbulat@gmail.com>
2014-03-25 17:31:51 +0000

    update rails testing fixture to rails 4

--- fixture_rails_root/config/initializers/secret_token.rb
+++ fixture_rails_root/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FixtureRailsRoot::Application.config.secret_token = '26d11ce8a1e3c4fe2cf38214eb81a0c36c6ba952b144bb601553bb3026f532d79468ae3029a401efbdae9128a51e67bf866f29d0a097640308d8d70a3443c83a'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FixtureRailsRoot::Application.config.secret_key_base = '09ac210bc16ac3bda117a667f3c5cc3f73ba26c9789df9ca2ce3ad42f73f4fc0cf356fa182bf34852e2b2912d83ad09a44ab031708c20bb88175190f20f351e9'
